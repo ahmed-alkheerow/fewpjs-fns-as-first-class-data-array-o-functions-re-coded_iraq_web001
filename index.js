6aa8a6a04b9e3fe6f59c26e928cd349c4491e99f
Ahmed Al-kheerow <a.alkheerow@gmail.com>
2020-05-20 02:15:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,23 @@ function wakeDog(dogName, dogBreed){
   return `Wake ${dogName} the ${dogBreed}`
 
 }
- function leashDog() {
+ function leashDog(dogName, dogBreed) {
+     return `Leash ${dogName} the ${dogBreed}`
+
+ }
+ function walkToPark(dogName, dogBreed) {
+     return `walk To Park ${dogName} the ${dogBreed}`
+
+ }
+ function throwFrisbee(dogName, dogBreed) {
+     return `Throw frisbee ${dogName} the ${dogBreed}`
+
+ }
+ function walkHome(dogName, dogBreed) {
+     return `Walk home ${dogName} the ${dogBreed}`
+
+ }
+ function unleashDog(dogName, dogBreed) {
+     return `Unleash  ${dogName} the ${dogBreed}`
 
  }
